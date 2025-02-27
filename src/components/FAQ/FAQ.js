b316d1c4a708cef956a1b8b381e815ea6052f128
Pedr0ff <156836814+Pedr0ff@users.noreply.github.com>
2025-02-27 19:24:10 +0100

    Update FAQ.js

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -3,24 +3,24 @@ import React from 'react';
 const FAQ = () => {
   const faqData = [
     {
-      question: "What services do you offer?",
-      answer: "I offer a range of services including web design, branding, digital marketing, UI/UX design etc. Each project is tailored to meet your unique needs and goals.",
+      question: "Quels services proposez-vous ?",
+      answer: "J'offre une gamme de services comprenant la conception web, le branding, le marketing numérique, la conception UI/UX, etc. Chaque projet est adapté pour répondre à vos besoins et objectifs uniques.",
     },
     {
-      question: "How do I get started on a project with you?",
-      answer: "Getting started is simple! Just reach out via the contact form on my website with your project details, and I’ll schedule a consultation to discuss how we can work together to achieve your vision.",
+      question: "Comment puis-je commencer un projet avec vous ?",
+      answer: "C'est simple ! Contactez-moi via le formulaire de contact sur mon site web avec les détails de votre projet, et je programmerai une consultation pour discuter de la façon dont nous pouvons travailler ensemble pour réaliser votre vision.",
     },
     {
-      question: "Can you help with creative direction or ideas?",
-      answer: "Absolutely! Whether you have a clear vision or need help developing ideas, I offer creative direction to guide your project and ensure it reflects your brand or personal style.",
+      question: "Pouvez-vous m'aider avec la direction créative ou les idées ?",
+      answer: "Absolument ! Que vous ayez une vision claire ou que vous ayez besoin d'aide pour développer des idées, j'offre une direction créative pour guider votre projet et garantir qu'il reflète votre marque ou votre style personnel.",
     },
     {
-      question: "What is your pricing structure?",
-      answer: "Pricing varies depending on the scope and complexity of the project. After an initial discussion, I’ll provide you with a customized quote that aligns with your specific requirements.",
+      question: "Quelle est votre structure tarifaire ?",
+      answer: "Les prix varient en fonction de la portée et de la complexité du projet. Après une première discussion, je vous fournirai un devis personnalisé qui correspond à vos besoins spécifiques.",
     },
     {
-      question: "Do you work with international clients?",
-      answer: "Yes, I work with clients from around the world. Thanks to remote collaboration tools, we can easily communicate and work together regardless of your location.",
+      question: "Travaillez-vous avec des clients internationaux ?",
+      answer: "Oui, je travaille avec des clients du monde entier. Grâce aux outils de collaboration à distance, nous pouvons facilement communiquer et travailler ensemble, quel que soit votre emplacement.",
     },
   ];
 
@@ -30,7 +30,7 @@ const FAQ = () => {
         <div className="row justify-content-center">
           <div className="col-12 text-center">
             <div className="intro">
-              <h3 className="title">Frequently Asked Questions</h3>
+              <h3 className="title">Questions fréquentes</h3>
             </div>
           </div>
         </div>
